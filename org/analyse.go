cdd69cac8d7fdbfe7d60ed7ab3d42fa95b508311
Rigidus <avenger-f@yandex.ru>
2020-04-29 18:31:38 +0300

    analyse: refactoring

--- org/analyse.go
+++ org/analyse.go
@@ -14,10 +14,6 @@ import (
     "strings"
 )
 
-const (
-    filename  = "src/github.com/insolar/assured-ledger/ledger-core/v2/logicrunner/sm_object/object.go"
-)
-
 type ParsedFile struct {
     dbg      bool
     filename string
@@ -57,19 +53,25 @@ type Variant struct {
 
 
 func main () {
-    path := flag.String("f", filename, "Path to file")
-
-    uml := analyse(*path)
-    // uml += "uml" // rem
-    // fmt.Printf("\n") // rem
+    path := flag.String("f", "", "Path to file")
     console := flag.Bool("c", false, "Print uml diagram to console")
+    debug := flag.Bool("d", false, "Enable debug")
+    flag.Parse()
+
+    if *path == "" {
+        fmt.Print("Error: Path to file is not specific\n")
+        return
+    }
+    uml := analyse(*path, *debug)
+
     if *console {
         fmt.Printf("\n\n\n\n\n~~~~~~~~~~~~~~~~~\n%s", uml)
     }
+
     writeUml(*path, uml)
 }
 
-func isContainInSet (s1 []string, item string) bool {
+func isInSet (s1 []string, item string) bool {
     for i1 := range s1 {
         if s1[i1] == item {
             return true
@@ -78,12 +80,12 @@ func isContainInSet (s1 []string, item string) bool {
     return false
 }
 
-// SetDiff returns a new set with all the elements that exist in
+// setDiff returns a new set with all the elements that exist in
 // the first set and don't exist in the second set
-func SetDiff (s1 []string, s2 []string) []string {
+func setDiff (s1 []string, s2 []string) []string {
     ret := []string{}
     for i1 := range s1 {
-        if !isContainInSet(s2, s1[i1]) {
+        if !isInSet(s2, s1[i1]) {
            ret = append(ret, s1[i1])
         }
     }
@@ -93,22 +95,20 @@ func SetDiff (s1 []string, s2 []string) []string {
 func SetUniWoDup (s1 []string, s2 []string) []string {
     ret := s1
     for i2 := range s2 {
-        if !isContainInSet(s1, s2[i2]) {
+        if !isInSet(s1, s2[i2]) {
            ret = append(ret, s2[i2])
         }
     }
     return ret
 }
 
-func analyse(filename string) string {
-    debug := flag.Bool("d", false, "Enable debug")
-    pathname := fmt.Sprintf("%s/%s", os.Getenv("GOPATH"), filename)
-    pf := ParseFile(pathname, *debug)
+func analyse(path string, debug bool) string {
+    pf := ParseFile(path, debug)
     if nil == pf {
-        panic("Cannot parse file "+pathname)
+        panic("Cannot parse file " + path)
     }
     uml := "@startuml"
-    pf.diag("\n\n:: ======= resource filename: %s", pf.filename)
+    pf.dbgmsg("\n\n:: ======= resource filename: %s", pf.filename)
 
     unvisited := []string{"Init"}
     visited   := []string{}
@@ -121,10 +121,10 @@ func analyse(filename string) string {
         visited = append(visited, state.Name)
         // uml += fmt.Sprintf("\n:state.Migr:[%s]", state.Migr)
 
-        pf.diag("\n\nfn: %s", state.Name) // Function name
-        // pf.diag("\nrecv: %s | %s", state.Recv.Name, state.Recv.Type) // Receiver
+        pf.dbgmsg("\n\nfn: %s", state.Name) // Function name
+        // pf.dbgmsg("\nrecv: %s | %s", state.Recv.Name, state.Recv.Type) // Receiver
         for parName, parType := range state.Pars { // Parameters
-            pf.diag("\npar name: %s | type: %s", parName, parType)
+            pf.dbgmsg("\npar name: %s | type: %s", parName, parType)
         }
 
         // curr_migr propagate migration to outgoing arrows
@@ -142,8 +142,8 @@ func analyse(filename string) string {
 
         for _, pos_migr := range possible_migrations {
             uml += fmt.Sprintf("\n%s : %s", state.Name, pos_migr)
-            uml += fmt.Sprintf("\n%s -[#blue]-> %s", state.Name, pos_migr)  // TEMPORARY COMMENTED
-            if !isContainInSet(visited, pos_migr) {
+            uml += fmt.Sprintf("\n%s -[#blue]-> %s", state.Name, pos_migr)
+            if !isInSet(visited, pos_migr) {
                 unvisited = append(unvisited, pos_migr)
             }
         }
@@ -151,8 +151,8 @@ func analyse(filename string) string {
         for _, ret := range state.Rets {
             // uml += fmt.Sprintf("\n%s -[#green]-> %s", state.Name, ret)
 
-            pf.diag("\n%s: ['%s']", ret.Lvl, ret.Str)
-              pf.diag("\nfun: ['%s']\nobj: ['%s']", ret.Var.Fun, ret.Var.Obj)
+            pf.dbgmsg("\n%s: ['%s']", ret.Lvl, ret.Str)
+              pf.dbgmsg("\nfun: ['%s']\nobj: ['%s']", ret.Var.Fun, ret.Var.Obj)
               // dbg
               // uml += fmt.Sprintf("\n ! %s | %s", ret.StepMigration, ret.Var.Fun)
               switch ret.Var.Fun {
@@ -175,22 +175,22 @@ func analyse(filename string) string {
                   uml += fmt.Sprintf("\n%s -[#RoyalBlue]-> %s : RepeatOr(Jump)Else", state.Name, ret.Args[2].Fun)
                   uml += fmt.Sprintf("\n%s -[#DarkGreen]-> %s : RepeatOrJump(Else)", state.Name, ret.Args[3].Fun)
               default:
-                  pf.diag("\n(=> (. %s %s)", ret.Var.Obj, ret.Var.Fun)
+                  pf.dbgmsg("\n(=> (. %s %s)", ret.Var.Obj, ret.Var.Fun)
                   for _, arg := range ret.Args {
-                      pf.diag("\n       %s", fmt.Sprintf("(. %s %s)", arg.Obj, arg.Fun))
+                      pf.dbgmsg("\n       %s", fmt.Sprintf("(. %s %s)", arg.Obj, arg.Fun))
                   }
-                  pf.diag(")")
+                  pf.dbgmsg(")")
               }
 
               // -:- fn representation
-              pf.diag(fmt.Sprintf("\n(-> (. %s %s)", ret.Var.Obj, ret.Var.Fun))
+              pf.dbgmsg(fmt.Sprintf("\n(-> (. %s %s)", ret.Var.Obj, ret.Var.Fun))
 
               for _, arg := range ret.Args {
-                  pf.diag("\n       %s", fmt.Sprintf("(. %s %s)", arg.Obj, arg.Fun))
+                  pf.dbgmsg("\n       %s", fmt.Sprintf("(. %s %s)", arg.Obj, arg.Fun))
               }
-              pf.diag(")")
+              pf.dbgmsg(")")
         }
-        unvisited = SetDiff(unvisited, visited)
+        unvisited = setDiff(unvisited, visited)
     }
 
     state_keys := make([]string, 0, len(pf.states))
@@ -198,53 +198,18 @@ func analyse(filename string) string {
         state_keys = append(state_keys, k)
     }
 
-    // THIS IS TEMPORARY DUPLICATE CODE FOR DEBUG PURPROSES
-    for _, state_key := range SetDiff(state_keys, visited) {
-        uml += fmt.Sprintf("\n%s : UNVISITED", state_key)
-
-        state := pf.states[state_key]
-
-        for _, ret := range state.Rets {
-            switch ret.Var.Fun {
-            case "Stop":
-                uml += fmt.Sprintf("\n%s --> [*]", state.Name)
-            case "Jump", "ThenJump":
-                uml += fmt.Sprintf("\n%s --> %s : %s", state.Name, ret.Args[0].Fun, ret.Var.Fun)
-                unvisited = append(unvisited, ret.Args[0].Fun)
-                // if nil != possible_migrations {
-                //     pf.states[ret.Args[0].Fun].Prop = append(pf.states[ret.Args[0].Fun].Prop, possible_migrations...)
-                // }
-            case "JumpExt":
-                uml += fmt.Sprintf("\n%s --> %s : %s", state.Name, ret.Args[0].Fun, ret.Var.Fun)
-                unvisited = append(unvisited, ret.Args[0].Fun)
-                uml += fmt.Sprintf("\n%s -[#DarkGreen]-> %s : %s-(StepMigration)", state.Name, ret.StepMigration, ret.Var.Fun)
-                unvisited = append(unvisited, ret.StepMigration)
-            case "ThenRepeat":
-                uml += fmt.Sprintf("\n%s --> %s : ThenRepeat", state.Name, state.Name)
-            case "RepeatOrJumpElse":
-                uml += fmt.Sprintf("\n%s -[#RoyalBlue]-> %s : RepeatOr(Jump)Else", state.Name, ret.Args[2].Fun)
-                uml += fmt.Sprintf("\n%s -[#DarkGreen]-> %s : RepeatOrJump(Else)", state.Name, ret.Args[3].Fun)
-            default:
-                pf.diag("\n(=> (. %s %s)", ret.Var.Obj, ret.Var.Fun)
-                for _, arg := range ret.Args {
-                    pf.diag("\n       %s", fmt.Sprintf("(. %s %s)", arg.Obj, arg.Fun))
-                }
-                pf.diag(")")
-            }
-        }
-    }
     uml += "\n@enduml\n"
     return uml
 }
 
-func writeUml(path string, uml string)  {
+func writeUml(path string, uml string) {
     name := filepath.Base(path)
-    name = strings.Replace(name, ".go", "", -1)+".plantuml"
-    umlPath := fmt.Sprintf("%s/%s/%s", os.Getenv("GOPATH"), filepath.Dir(path), name)
+    name = strings.Replace(name, ".go", "", -1) + ".plantuml"
+    umlPath := fmt.Sprintf("%s/%s", filepath.Dir(path), name)
 
     file, err := os.Create(umlPath)
     if err != nil {
-        fmt.Printf("Failed to create file: %s\n", umlPath)
+        fmt.Printf("%s Failed to create file: %s \n", err.Error(), umlPath)
         return
     }
 
@@ -265,14 +230,15 @@ func writeUml(path string, uml string)  {
     fmt.Printf("Uml saved: %s\n", umlPath)
 }
 
-func ParseFile(fileName string, dbg ...bool) *ParsedFile {
+func ParseFile(path string, dbg ...bool) *ParsedFile {
     pf := &ParsedFile{
-        filename: fileName,
+        filename: path,
         dbg:      dbg[0],
     }
 
-    sourceCode, err := slurpFile(fileName)
+    sourceCode, err := slurpFile(path)
     if err != nil {
+        fmt.Printf("Failed to slurpFile file: %s %s\n", path, err.Error())
         return nil
     }
     pf.code = sourceCode
@@ -280,6 +246,7 @@ func ParseFile(fileName string, dbg ...bool) *ParsedFile {
     pf.fileSet = token.NewFileSet()
     node, err := parser.ParseFile(pf.fileSet, pf.filename, pf.code, parser.ParseComments)
     if err != nil {
+        fmt.Printf("Failed to parse file: %s\n", path)
         return nil
     }
     pf.node = node
@@ -297,16 +264,16 @@ func ParseFile(fileName string, dbg ...bool) *ParsedFile {
     return pf
 }
 
-func slurpFile(fileName string) ([]byte, error) {
-    file, err := os.OpenFile(fileName, os.O_RDONLY, 0)
+func slurpFile(filename string) ([]byte, error) {
+    file, err := os.OpenFile(filename, os.O_RDONLY, 0)
     if err != nil {
-        return nil, errors.New(fmt.Sprintf("Can't open file: [%s]", filename))
+        return nil, errors.New(fmt.Sprintf("Can't open file: [%s], %s\n", filename, err.Error()))
     }
     defer file.Close() //nolint: errcheck
 
     res, err := ioutil.ReadAll(file)
     if err != nil {
-        return nil, errors.New(fmt.Sprintf("Can't read file: [%s]", filename))
+        return nil, errors.New(fmt.Sprintf("Can't read file: [%s]\n", filename))
     }
     return res, nil
 }
@@ -315,7 +282,7 @@ func (pf *ParsedFile) parseMethod(fn *ast.FuncDecl) {
 
     // I want to analise only method functions (if exists)
     if nil == fn.Recv {
-        pf.diag("\n:parseMethod: skip %s - No receiver", fn.Name.Name)
+        pf.dbgmsg("\n:parseMethod: skip %s - No receiver", fn.Name.Name)
     } else {
         for _, fld := range fn.Recv.List {
             pf.parseRecv(fn, fld)
@@ -344,13 +311,13 @@ func (pf *ParsedFile) parseRecv(fn *ast.FuncDecl, fld *ast.Field) {
 
     // I want to analyse only methods, who takes context
     if !isMethodTakesCtx(pars) {
-        pf.diag("\n:parseMethod: skip %s - Doesn`t take CTX", fn.Name.Name)
+        pf.dbgmsg("\n:parseMethod: skip %s - Doesn`t take CTX", fn.Name.Name)
         return
     }
 
     // I want analyse only methods, which returned values
     if nil == fn.Type.Results {
-        pf.diag("\n:parseMethod: skip %s - No return value", fn.Name.Name)
+        pf.dbgmsg("\n:parseMethod: skip %s - No return value", fn.Name.Name)
         return
     }
 
@@ -372,7 +339,7 @@ func (pf *ParsedFile) parseRecv(fn *ast.FuncDecl, fld *ast.Field) {
     }
 
     // Show name (debug)
-    pf.diag("\n:parseMethod: (sm-name) %s", fn.Name.Name)
+    pf.dbgmsg("\n:parseMethod: (sm-name) %s", fn.Name.Name)
 
     // Find all Return Statements and SetDefaultMigration calls
     var rets = make([]*Ret, 0)
@@ -421,12 +388,12 @@ func (pf *ParsedFile) findSetDefaultMigration (migr string, in ast.Node) string 
                         for _, arg := range callexpr.Args {
                             argsel, ok := arg.(*ast.SelectorExpr)
                             if ok {
-                                pf.diag(fmt.Sprintf("\n>>>:[%s]", argsel.Sel.Name))
+                                pf.dbgmsg(fmt.Sprintf("\n>>>:[%s]", argsel.Sel.Name))
                                 migr = argsel.Sel.Name
                             }
                             argnil, ok := arg.(*ast.Ident)
                             if ok {
-                                pf.diag(fmt.Sprintf("\n>>>:[%s]", argnil))
+                                pf.dbgmsg(fmt.Sprintf("\n>>>:[%s]", argnil))
                                 migr = "NIL"
                             }
                         }
@@ -438,7 +405,7 @@ func (pf *ParsedFile) findSetDefaultMigration (migr string, in ast.Node) string 
     return migr
 }
 
-func (pf *ParsedFile) diag(msg string, par ...interface{}) {
+func (pf *ParsedFile) dbgmsg(msg string, par ...interface{}) {
     if pf.dbg {
         fmt.Printf(msg, par...)
     }
@@ -460,7 +427,7 @@ func (pf *ParsedFile) collectRets(retStmt *ast.ReturnStmt, level string) []*Ret 
             Lvl: level,
             Str: fmt.Sprintf("%s", pf.code[ret.Pos()-1:ret.End()-1]),
         }
-        pf.diag("\n :collectRet: ~~~~~~ (item.Str) : %s",  item.Str)
+        pf.dbgmsg("\n :collectRet: ~~~~~~ (item.Str) : %s",  item.Str)
 
         for _, retNode := range retStmt.Results {
             switch retNode.(type) {
@@ -470,23 +437,23 @@ func (pf *ParsedFile) collectRets(retStmt *ast.ReturnStmt, level string) []*Ret 
                 case *ast.SelectorExpr:
                     retSelector := retCall.Fun.(*ast.SelectorExpr)
                     item.Var.Fun = retSelector.Sel.Name
-                    pf.diag("\n  :collectRet: (Selector) (%s.) =:[%s]:=", reflect.TypeOf(retSelector.X), retSelector.Sel.Name)
+                    pf.dbgmsg("\n  :collectRet: (Selector) (%s.) =:[%s]:=", reflect.TypeOf(retSelector.X), retSelector.Sel.Name)
                     switch retSelector.X.(type) { // Analyse started from [selector.*]
                     case *ast.Ident:
                         retX := retSelector.X.(*ast.Ident)
                         item.Var.Obj = retX.Name
-                        pf.diag("\n   :collectRet: (ident) : %s _._", item.Var.Obj)
+                        pf.dbgmsg("\n   :collectRet: (ident) : %s _._", item.Var.Obj)
                         switch item.Var.Fun {
                         case "Jump", "Stop", "JumpExt":
                         default:
-                            pf.diag("\n:collectRets: [WARN]: UNKNOWN RET SELECTOR '%s' in '%s.%s'",
+                            pf.dbgmsg("\n:collectRets: [WARN]: UNKNOWN RET SELECTOR '%s' in '%s.%s'",
                                 item.Var.Fun, item.Var.Obj, item.Var.Fun)
                         }
                     case *ast.CallExpr:
                         subX := retSelector.X.(*ast.CallExpr)
                         subXStr := fmt.Sprintf("%s", pf.code[subX.Pos()-1:subX.End()-1])
                         item.Var.Obj = subXStr
-                        pf.diag("\n   :collectRet: (call to selector) : %s _._", item.Var.Obj)
+                        pf.dbgmsg("\n   :collectRet: (call to selector) : %s _._", item.Var.Obj)
                         switch item.Var.Fun { // Check Fun (nb: not arg!)
                         case "ThenRepeat", "ThenJump":
                         default:
@@ -503,12 +470,12 @@ func (pf *ParsedFile) collectRets(retStmt *ast.ReturnStmt, level string) []*Ret 
                     // Args
                     accArgs := make([]Variant, 0)
                     for _, retarg := range retCall.Args {
-                        pf.diag("\n   -:collectRet: arg type [%s]", reflect.TypeOf(retarg))
+                        pf.dbgmsg("\n   -:collectRet: arg type [%s]", reflect.TypeOf(retarg))
                         switch retarg.(type) {
                         case *ast.SelectorExpr:
                             sel := retarg.(*ast.SelectorExpr)
                             selName := fmt.Sprintf("%s", pf.code[sel.X.Pos()-1:sel.X.End()-1])
-                            pf.diag("\n   -|[%s] %s .|. %s", reflect.TypeOf(sel), selName, sel.Sel.Name)
+                            pf.dbgmsg("\n   -|[%s] %s .|. %s", reflect.TypeOf(sel), selName, sel.Sel.Name)
                             arg := Variant{
                                 Obj:  selName,
                                 Fun:  sel.Sel.Name,
@@ -531,7 +498,7 @@ func (pf *ParsedFile) collectRets(retStmt *ast.ReturnStmt, level string) []*Ret 
                                                 Obj:  selName,
                                                 Fun:  sel.Sel.Name,
                                             }
-                                            pf.diag("\n   -| -transition: %s.%s", selName, sel.Sel.Name)
+                                            pf.dbgmsg("\n   -| -transition: %s.%s", selName, sel.Sel.Name)
                                         case "Migration":
                                             sel := exp.Value.(*ast.SelectorExpr)
                                             selName := fmt.Sprintf("%s", pf.code[sel.X.Pos()-1:sel.X.End()-1])
@@ -541,27 +508,27 @@ func (pf *ParsedFile) collectRets(retStmt *ast.ReturnStmt, level string) []*Ret 
                                             //     Obj:  selName,
                                             //     Fun:  sel.Sel.Name,
                                             // }
-                                            pf.diag("\n   -| --migration: %s.%s", selName, sel.Sel.Name)
+                                            pf.dbgmsg("\n   -| --migration: %s.%s", selName, sel.Sel.Name)
                                         default:
-                                            pf.diag("\n:collectRets: [ERR]: UNKNOWN keystr [%s]", keystr)
+                                            pf.dbgmsg("\n:collectRets: [ERR]: UNKNOWN keystr [%s]", keystr)
                                         }
                                     }
                                     return true
                                 }) // end of Inspect
                             } else {
-                                pf.diag("\n:collectRets: [ERR]: UNK JumpExt transition")
+                                pf.dbgmsg("\n:collectRets: [ERR]: UNK JumpExt transition")
                             }
                             accArgs = append(accArgs, arg)
                         default:
-                            pf.diag("\n:collectRets: [ERR]: UNKNOWN RETARGtype [%s] :OF: %s", reflect.TypeOf(retarg), retarg)
+                            pf.dbgmsg("\n:collectRets: [ERR]: UNKNOWN RETARGtype [%s] :OF: %s", reflect.TypeOf(retarg), retarg)
                         }
                     } // end of args
                     item.Args = accArgs
                 default:
-                    pf.diag("\n:collectRets: [ERR]: UNKNOWN RETSEL %s", fmt.Sprintf("%s", reflect.TypeOf(retCall.Fun)))
+                    pf.dbgmsg("\n:collectRets: [ERR]: UNKNOWN RETSEL %s", fmt.Sprintf("%s", reflect.TypeOf(retCall.Fun)))
                 }
             default:
-                pf.diag("\n [ERR]: UNKNOWN TYPE OF RETNODE %s", fmt.Sprintf("%s", reflect.TypeOf(retNode)))
+                pf.dbgmsg("\n [ERR]: UNKNOWN TYPE OF RETNODE %s", fmt.Sprintf("%s", reflect.TypeOf(retNode)))
             } // end of switch retnode type
         }
         acc = append(acc, item)
